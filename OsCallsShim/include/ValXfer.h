24cf4f9cf28a7cf112f52c7a651be81be77d0011
Kai Henningsen <kai.extern+git@gmail.com>
2025-10-31 02:42:26 +0100

    style: apply C++ formatting to ValXfer files
    - Change pointer declaration style from 'Type *name' to 'Type* name'
    - Move extern "C" opening brace to same line
    - Adjust indentation of function definitions in extern "C" block

--- OsCallsShim/include/ValXfer.h
+++ OsCallsShim/include/ValXfer.h
@@ -9,14 +9,14 @@ namespace OsCalls
 {
     struct ValueT;
 
-    typedef bool HandlerT(ValueT *value);
+    typedef bool HandlerT(ValueT* value);
 
     // Define the THandle struct
     struct HandleT
     {
-        HandlerT *handler;
-        void *data1;
-        void *data2;
+        HandlerT* handler;
+        void* data1;
+        void* data2;
         int64_t index;
     };
 
@@ -37,17 +37,16 @@ namespace OsCalls
         HandleT Handle;
         timespec TimeSpec;
         int64_t Number;
-        const char *Name;
-        const char *String;
-        ValueT *Complex;
+        const char* Name;
+        const char* String;
+        ValueT* Complex;
         TypeT Type;
     };
 
     // Declare the external C functions
-    extern "C"
-    {
-        bool GetNextValue(ValueT *value);
-        void CreateHandle(ValueT *value, HandlerT *handler, void *data1, void *data2);
+    extern "C" {
+    bool GetNextValue(ValueT* value);
+    void CreateHandle(ValueT* value, HandlerT* handler, void* data1, void* data2);
     }
 } // namespace OsCalls
 
--- OsCallsShim/src/ValXfer.cpp
+++ OsCallsShim/src/ValXfer.cpp
@@ -2,27 +2,26 @@
 
 namespace OsCalls
 {
-    extern "C"
+    extern "C" {
+    bool GetNextValue(ValueT* value)
     {
-        bool GetNextValue(ValueT *value)
-        {
-            auto ret = value->Handle.handler(value);
-            value->Handle.index++;
-            return ret;
-        };
+        auto ret = value->Handle.handler(value);
+        value->Handle.index++;
+        return ret;
+    };
 
-        void CreateHandle(ValueT *value, HandlerT *handler, void *data1, void *data2)
-        {
-            value->Handle.handler = handler;
-            value->Handle.data1 = data1;
-            value->Handle.data2 = data2;
-            value->Handle.index = 0;
-            value->Type = TypeT::IsError;
-            value->Name = "errno";
-            value->Number = 0;
-            value->String = nullptr;
-            value->Complex = nullptr;
-            value->TimeSpec = {0, 0};
-        };
+    void CreateHandle(ValueT* value, HandlerT* handler, void* data1, void* data2)
+    {
+        value->Handle.handler = handler;
+        value->Handle.data1 = data1;
+        value->Handle.data2 = data2;
+        value->Handle.index = 0;
+        value->Type = TypeT::IsError;
+        value->Name = "errno";
+        value->Number = 0;
+        value->String = nullptr;
+        value->Complex = nullptr;
+        value->TimeSpec = {0, 0};
+    };
     }
 } // namespace OsCalls
